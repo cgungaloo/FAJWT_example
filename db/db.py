feadd632795602ef97d0195ea79b707aa97394ca
cgungaloo <cgungaloo@yahoo.com>
2024-02-11 21:03:32 +0000

    init commit

---
+++ db/db.py
@@ -0,0 +1,14 @@
+
+SECRET_KEY = "89d6a4413c149619b603e4c8dde1f31c487c37e87c08bcacc4bfd1e2b2e82ebe"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
